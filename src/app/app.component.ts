35b361bf7c0a922386108d66192ace4556dbf573
Nitanshu <nitanshu@Nitanshus-MacBook-Air.local>
2019-01-02 00:52:59 +0530

    futher changes in map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MeetikzService } from './core/services/meetinkz.service';
-
+import { cloneDeep } from 'lodash';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +9,14 @@ import { MeetikzService } from './core/services/meetinkz.service';
 export class AppComponent implements OnInit {
   title = 'meetinkz';
   allMeetings: any = [];
+  allMeetingsClone: any = [];
 
   lat = 51.5073509;
   lng = -0.12775829999998223;
-  zoom = 8;
+  zoom = 12;
   markers = [];
   markerImg = 'http://team-scale.com/TestData/ng_text_v15/blue_marker.png';
-  openedWindow: number = 0;
+  openedWindow = 0;
 
   constructor(private meetingService: MeetikzService) {
 
@@ -25,16 +26,12 @@ export class AppComponent implements OnInit {
     this.meetingService.getAllMeeting().subscribe(res => {
       const result: any = res;
       this.allMeetings = result;
-      result.forEach(element => {
-        this.markers.push({
-          lat: element.lat,
-          lng: element.lon,
-          url: this.markerImg,
-          name: element.name,
-          id: element.id
-        });
-      });
+      this.allMeetingsClone = cloneDeep(result);
+      this.mapTheMarkers(this.allMeetings);
     });
+  }
+  radiusChanged(number) {
+    console.log(number);
   }
 
   locateOnMap(meeting, index) {
@@ -43,15 +40,57 @@ export class AppComponent implements OnInit {
       elm.url = this.markerImg;
     });
     this.markers[index].url = 'http://team-scale.com/TestData/ng_text_v15/orange_marker.png';
+    this.markers[index].open = true;
   }
 
   clickedMarker(index: number, id) {
-    console.log(`clicked the marker: ${index}`);
-    this.markers[index].url = 'http://team-scale.com/TestData/ng_text_v15/orange_marker.png';
-    this.openedWindow = id;
+    if (this.markers[index].open === false) {
+      console.log(`clicked the marker: ${index}`);
+      this.markers[index].url = 'http://team-scale.com/TestData/ng_text_v15/orange_marker.png';
+      this.openedWindow = id;
+      this.markers[index].open = true;
+      const searchProperty = cloneDeep(this.allMeetings.filter((list) => {
+        return list['id'] === id;
+      }));
+      this.allMeetings = searchProperty;
+    } else {
+      this.markers[index].url = 'http://team-scale.com/TestData/ng_text_v15/blue_marker.png';
+      this.openedWindow = id;
+      this.markers[index].open = false;
+      this.allMeetings = cloneDeep(this.allMeetingsClone);
+    }
   }
+
   mapClicked() {
 
+  }
+  mapTheMarkers(meetings) {
+    this.markers = [];
+    meetings.forEach(element => {
+      this.markers.push({
+        lat: element.lat,
+        lng: element.lon,
+        url: this.markerImg,
+        name: element.name,
+        id: element.id,
+        open: false
+      });
+    });
+  }
+  onSearch(event: any) {
+    const term = event.target.value;
+
+    if (term.length > 2) {
+      const searchProperty = cloneDeep(this.allMeetings.filter((list) => {
+        return list['name'].toLowerCase().indexOf(term.toLowerCase()) > -1;
+      }));
+      this.allMeetings = searchProperty;
+      this.mapTheMarkers(this.allMeetings);
+    } else {
+
+      this.allMeetings = cloneDeep(this.allMeetingsClone);
+      this.mapTheMarkers(this.allMeetings);
+    }
   }
 
 
